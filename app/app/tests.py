3c1df300a9a69c99b8bc111b732668f46e5c3d11
Mateusz Lesniak <lm48134@zut.edu.pl>
2025-02-10 17:21:33 +0100

    Initial db configuration

---
+++ app/app/tests.py
@@ -0,0 +1,17 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+    def test_add(self):
+        """Test adding two numbers"""
+        self.assertEqual(calc.add(3, 8), 11)
+
+    def test_subtract(self):
+        """Test subtracting two numbers"""
+        self.assertEqual(calc.subtract(5, 11), 6)
